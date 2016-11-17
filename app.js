92c0b8848e14fa7b62e735e13202845b8dba53c4
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-17 13:07:01 +0000

    Setting up DB configuration

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express"),
+    mongoose = require("mongoose"),
+    Latest = require("./models/latest"),
     app     = express();
+    
+    
+mongoose.Promise = global.Promise;
+
+//for local development
+mongoose.connect("mongodb://localhost/imagesearch");
+
+//on mongo lab
+//mongoose.connect("mongodb://image-search-api:image-search-api@ds157247.mlab.com:57247/image-search-api");
+   
     
 app.set("view engine", "ejs");
 app.get("/",function(req,res){
