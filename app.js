d83ecf74f15ecb7e507d874e1b0b03ec5278f739
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-17 13:08:49 +0000

    Added search route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     mongoose = require("mongoose"),
     Latest = require("./models/latest"),
+    bingSearch = require("bing.search"),
     app     = express();
     
     
@@ -12,11 +13,35 @@ mongoose.connect("mongodb://localhost/imagesearch");
 //on mongo lab
 //mongoose.connect("mongodb://image-search-api:image-search-api@ds157247.mlab.com:57247/image-search-api");
    
+var search = new bingSearch("B19fx6nXcipELSH4uCwxcdxKHhhogPZx4HydA4bcUqc");
     
 app.set("view engine", "ejs");
 app.get("/",function(req,res){
     
     res.render("index");
+});
+
+app.get("/search/:query",function(req,res){
+    var query = req.params.query;
+    var offset = req.params.offset || 10;
+    var info = {query:query};
+    search.images(query, {top : offset}, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            //console.log(result);
+            res.json(result);
+        }
+    });
+
+    Latest.create(info,function(err,data){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(data);
+        }
+    });
+
 });
 
 app.listen(process.env.PORT, process.env.IP);
